30bb0fadcdac8eb61d9e32137db16404b769f2d0
rolies106 <rolies106@gmail.com>
2015-05-21 11:43:59 +0700

    Add conditional if php-redis installed

--- components/yii-resque/Worker/ClassWorker.php
+++ components/yii-resque/Worker/ClassWorker.php
@@ -7,18 +7,15 @@ class Worker_ClassWorker
     public function setUp()
     {
         # Set up environment for this job
-        echo "Setting up\n";
     }
 
     public function perform()
     {
         # Run task
-        echo "Running\n";
     }
 
     public function tearDown()
     {
         # Remove environment for this job
-        echo "Tear down\n";
     }
 }
